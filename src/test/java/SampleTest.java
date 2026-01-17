835ed558fcef7a3e10100b2bf64891adaa2d2a67
Maciej317 <maciej.glowacki04@onet.com.pl>
2026-01-17 18:54:28 +0100

    trdsa

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,38 @@
+import org.example.Calculator;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test1(){
+        int a = 4;
+        int b = 6;
+        assert a + b == 10;
+    }
+
+    @Test
+    public void test2(){
+        int x = 9;
+        assert x % 2 != 0;
+    }
+
+    @Test
+    public void test3(){
+        int x = 8;
+        assert x / 2 == 4;
+    }
+
+    @Test
+    public void test4(){
+        Calculator cal = new Calculator();
+        int result = cal.add(3, 7);
+        assert result == 10;
+    }
+
+    @Test
+    public void test5(){
+        Calculator cal = new Calculator();
+        boolean result = cal.isEven(6);
+        assert result == true;
+    }
+}
